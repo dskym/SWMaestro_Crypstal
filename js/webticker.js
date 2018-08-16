19d00cce3a27866c5659d395c645b69e964b22be
SeungYoon Kim <dskym@naver.com>
2018-08-16 18:38:19 +0900

    update webticker

--- js/webticker.js
+++ js/webticker.js
@@ -1,13 +1,41 @@
 $(function () {
     "use strict";
 
-    if ($('#webticker-6').length) {
-        $("#webticker-6").webTicker({
-            height:'auto',
-            duplicate:true,
-            startEmpty:false,
-            rssfrequency:5,
-            direction: 'right'
-        });
-    }
+    init();
+
+    $("#webticker").webTicker({
+        height:'auto',
+        duplicate:true,
+        startEmpty:false,
+        rssfrequency:5,
+        direction: 'right'
+    });
 });
+
+function init() {
+    var webTickerData = new Object();
+
+    var coinData = new Object();
+
+    coinData.btc = '$11.039232';
+    coinData.eth = '$1.2792';
+    coinData.game = '$11.039232';
+    coinData.lbc = '$0.588418';
+    coinData.neo = '$161.511';
+    coinData.ste = '$0.551955';
+    coinData.lit = '$177.80';
+    coinData.note = '$13.399';
+    coinData.mint = '$0.880694';
+    coinData.iot = '$2.555';
+    coinData.das = '$769.22';
+
+    webTickerData.exchange = 'upbit';
+    webTickerData.coin = coinData;
+
+    console.log(webTickerData);
+    console.log(webTickerData['exchange']);
+    console.log(webTickerData['coin']);
+    $.each(webTickerData['coin'], function(key, value) {
+        $("#webticker").append('<li><i class=\"cc BTC\"></i> ' + key + ' <span class=\"text-yellow\"> ' + value + '</span></li>');
+    });
+}
