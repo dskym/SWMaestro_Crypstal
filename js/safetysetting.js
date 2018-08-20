774117affd8ac5d8ce3c3516ae813a538ea4ee28
SeungYoon Kim <dskym@naver.com>
2018-08-20 09:57:58 +0900

    add safety UI

---
+++ js/safetysetting.js
@@ -0,0 +1,7 @@
+$(function () {
+    "use strict";
+
+
+    //botSettingInfo.botalarm = $('.bot-list #bot-alarm').is(":checked");
+    //botSettingInfo.autotrade = $('.bot-list #auto-trade').is(":checked");
+});
